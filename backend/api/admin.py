6bec499360d9516aba84986a986fad3b67e25ff0
Utsav Pati <utsavpati2012@gmail.com>
2021-11-21 23:41:08 +0530

    added the backend models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Guest)
+class Guestadmin(admin.ModelAdmin):
+    list_display=['id','name','duration','advanced_paid','room_choices','arrival']
+
+@admin.register(room_booked)
+class roomBookedadmin(admin.ModelAdmin):
+    list_display=['guest','room_number','unique_token_number']
+
+@admin.register(catering_orders)
+class cateringOrdersadmin(admin.ModelAdmin):
+    list_display=['guest','breakfast_quantity','lunch_quantity','snacks_quantity','dinner_quantity']
+
+@admin.register(room_charges)
+class roomChargesadmin(admin.ModelAdmin):
+    list_display=['room_type','room_charges']
+
+@admin.register(hotel)
+class hoteladmin(admin.ModelAdmin):
+    list_display=['room_type','room_availability']
+
+@admin.register(frequent_users)
+class frequentUsersadmin(admin.ModelAdmin):
+    list_display=['guest','frequency','UIN']
+
+
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+room_choices=(('non_ac_single','non_ac_single'),
+                ('non_ac_double','non_ac_double'),
+                ('ac_single','ac_single'),
+                ('ac_double','ac_double'))
+
+class Guest(models.Model):
+    name = models.CharField(max_length=50)
+    duration=models.IntegerField()
+    advanced_paid=models.IntegerField()
+    room_choices=models.CharField(max_length=50,choices=room_choices,default='Pending')
+    arrival=models.DateTimeField(auto_now_add=True)
+
+
+class room_booked(models.Model):
+    guest=models.OneToOneField(Guest,on_delete=models.CASCADE)
+    room_number=models.IntegerField()
+    unique_token_number=models.IntegerField(unique=True)
+
+
+
+class catering_orders(models.Model):
+    guest=models.OneToOneField(Guest,on_delete=models.CASCADE)
+    breakfast_quantity=models.IntegerField(default=0)
+    lunch_quantity=models.IntegerField(default=0)
+    snacks_quantity=models.IntegerField(default=0)
+    dinner_quantity=models.IntegerField(default=0)
+
+class room_charges(models.Model):
+    room_type=models.CharField(max_length=50,choices=room_choices)
+    room_charges=models.IntegerField()
+
+class hotel(models.Model):
+    room_type=models.CharField(max_length=50,choices=room_choices)
+    room_availability=models.IntegerField()
+
+class frequent_users(models.Model):
+   guest=models.CharField(max_length=50)
+   frequency=models.IntegerField()
+   UIN=models.IntegerField(unique=True)
